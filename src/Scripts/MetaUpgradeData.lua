7689a816601b109223e02fba670e7f3f20a2a273
ryanmaietta <ryanamaietta@gmail.com>
2025-10-14 14:06:49 -0700

    Revert to modutil.once_loaded.game

--- src/Scripts/MetaUpgradeData.lua
+++ src/Scripts/MetaUpgradeData.lua
@@ -1,2 +1,2 @@
-save.MetaUpgradeData.RerollCosts.Hammer = 1
-save.MetaUpgradeData.RerollCosts.ReuseIncrement = 0
+game.MetaUpgradeData.RerollCosts.Hammer = 1
+game.MetaUpgradeData.RerollCosts.ReuseIncrement = 0
