e9a83ea2b8442a7afb59368578d52b1c5ea2a27d
ryanmaietta <ryanamaietta@gmail.com>
2025-10-14 14:11:10 -0700

    Fix RerollCosts path and update version number in thunderstore.toml

--- src/Scripts/MetaUpgradeData.lua
+++ src/Scripts/MetaUpgradeData.lua
@@ -1,4 +1,4 @@
-game.MetaUpgradeData.RerollCosts.Hammer = 1
-game.MetaUpgradeData.RerollCosts.ReuseIncrement = 0
+game.RerollCosts.Hammer = 1
+game.RerollCosts.ReuseIncrement = 0
 
 game.SetupRunData()
